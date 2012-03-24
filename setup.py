e63f5b86781c7ad27105de50aaabe140856f93be
John Anderson <sontek@gmail.com>
2012-03-24 14:48:42 -0500

    bump version for another release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (2, 7):
     requires.append('argparse')
 
 setup(name='pyramid_celery',
-      version='1.1',
+      version='1.2',
       description='Celery integration with pyramid',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
