b76f06da1ad33b67b386119f22c7a182ebc86e3d
Guillaume Gauvrit <guillaume@gauvr.it>
2013-09-09 22:46:28 +0200

    Fix celeryctl

--- pyramid_celery/commands/celeryctl.py
+++ pyramid_celery/commands/celeryctl.py
@@ -1,17 +1,10 @@
 from pyramid_celery.commands import CommandMixin
-from celery.bin.celeryctl import celeryctl as BaseCeleryCtl
-from celery.bin.celeryctl import help as BaseHelp
-
-
-class Help(BaseHelp):
-    option_list = BaseHelp.option_list[len(BaseCeleryCtl.preload_options):]
+from celery.bin.celery import CeleryCommand as BaseCeleryCtl
 
 
 class CeleryCtl(CommandMixin, BaseCeleryCtl):
     commands = BaseCeleryCtl.commands.copy()
-    commands['help'] = Help
     option_list = BaseCeleryCtl.option_list[len(BaseCeleryCtl.preload_options):]
-
 
 
 def main():
