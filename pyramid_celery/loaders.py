e590c03760958ff65a5ed1f1af34879a1aa58fce
John Anderson <sontek@gmail.com>
2014-12-27 22:32:20 -0800

    Another small refactor of beat config

--- pyramid_celery/loaders.py
+++ pyramid_celery/loaders.py
@@ -7,6 +7,54 @@ from pyramid.exceptions import ConfigurationError
 from functools import partial
 
 
+def crontab(value):
+    return celery.schedules.crontab(**value)
+
+
+def timedelta(value):
+    return datetime.timedelta(**value)
+
+
+SCHEDULE_TYPE_MAP = {
+    'crontab': crontab,
+    'timedelta': timedelta,
+    'integer': int
+}
+
+
+def get_beat_config(parser, section):
+    get = partial(parser.get, section)
+    has_option = partial(parser.has_option, section)
+
+    schedule_type = get('type')
+    schedule_value = json.loads(get('schedule'))
+
+    scheduler_cls = SCHEDULE_TYPE_MAP.get(schedule_type)
+
+    if scheduler_cls is None:
+        raise ConfigurationError(
+            'schedule type %s in section %s is invalid' % (
+                schedule_type,
+                section
+            )
+        )
+
+    schedule = scheduler_cls(schedule_value)
+
+    config = {
+        'task': get('task'),
+        'schedule': schedule,
+    }
+
+    if has_option('args'):
+        config['args'] = json.loads(get('args'))
+
+    if has_option('kwargs'):
+        config['kwargs'] = json.loads(get('kwargs'))
+
+    return config
+
+
 class INILoader(celery.loaders.base.BaseLoader):
     ConfigParser = configparser.SafeConfigParser
 
@@ -28,46 +76,13 @@ class INILoader(celery.loaders.base.BaseLoader):
             config_dict['CELERY_IMPORTS'] = split_imports
 
         beat_config = {}
-        beat_sections = []
 
         for section in self.parser.sections():
-            if section.startswith('celerybeat:'):
-                beat_sections.append(section)
-
-        for section in beat_sections:
-            get = partial(self.parser.get, section)
-            has_option = partial(self.parser.has_option, section)
-
-            schedule_type = get('type')
-            schedule_value = json.loads(get('schedule'))
-
-            if schedule_type == 'crontab':
-                schedule = celery.schedules.crontab(**schedule_value)
-            elif schedule_type == 'timedelta':
-                schedule = datetime.timedelta(**schedule_value)
-            elif schedule_type == 'integer':
-                schedule = int(schedule_value)
-            else:
-                raise ConfigurationError(
-                    'schedule type %s in section %s is invalid' % (
-                        schedule_type,
-                        section
-                    )
-                )
-
-            task_config = {
-                'task': get('task'),
-                'schedule': schedule,
-            }
-
-            if has_option('args'):
-                task_config['args'] = json.loads(get('args'))
-
-            if has_option('kwargs'):
-                task_config['kwargs'] = json.loads(get('kwargs'))
+            if not section.startswith('celerybeat:'):
+                continue
 
             name = section.split(':', 1)[1]
-            beat_config[name] = task_config
+            beat_config[name] = get_beat_config(self.parser, section)
 
         if beat_config:
             config_dict['CELERYBEAT_SCHEDULE'] = beat_config
