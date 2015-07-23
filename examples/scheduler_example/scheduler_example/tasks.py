c7eda9816a675b47bf8bfc9ae198597d5e23243e
John Anderson <sontek@gmail.com>
2015-07-23 14:45:19 -0700

    Add example of using crontab

--- examples/scheduler_example/scheduler_example/tasks.py
+++ examples/scheduler_example/scheduler_example/tasks.py
@@ -7,3 +7,10 @@ def get_date(*args, **kwargs):
     msg = app.conf['PYRAMID_REGISTRY'].settings['message']
 
     print(msg % datetime.utcnow())
+
+
+@app.task
+def get_date_2(*args, **kwargs):
+    msg = app.conf['PYRAMID_REGISTRY'].settings['message']
+
+    print('crontab: ' + msg % datetime.utcnow())
