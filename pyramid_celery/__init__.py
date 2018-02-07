ed30d5bc49c4b43e1ba8189fa285409d4f3659bf
G.Tjebbes <g.t@majerti.fr>
2018-02-07 09:13:29 +0100

    Fix #66 : Don't use bare except

--- pyramid_celery/__init__.py
+++ pyramid_celery/__init__.py
@@ -102,7 +102,7 @@ def on_preload_parsed(options, **kwargs):
             env = bootstrap(ini_location, options=options)
         else:
             env = bootstrap(ini_location)
-    except:
+    except Exception:
         import traceback
         traceback.print_exc()
         exit(-1)
